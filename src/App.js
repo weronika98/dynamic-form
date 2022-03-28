120c2b8b9c4c10e9969a1cf0bfcb660de74fcf63
brandon.vaughn <bvaughn@avail.io>
2022-03-28 12:29:39 -0700

    refactor of dynamic logic

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [formOne, setFormOne] = useState(true);
   const { fields, page_label } = elements ?? {};
   const formJSON = formOne ? formOneJSON : formTwoJSON;
+
   useEffect(() => {
     setElements(formJSON[0]);
   }, [formJSON]);
@@ -22,14 +23,54 @@ function App() {
   const handleChange = (id, event) => {
     const newElements = { ...elements };
     newElements.fields.forEach((field) => {
-      const { field_type, field_id } = field;
+      const {
+        field_type,
+        field_id,
+        functionDefinition: {
+          functionTrigger,
+          functionType,
+          targetField,
+          targetFieldValue,
+          newFieldDefinition: {
+            newField_id,
+            newField_label,
+            newField_mandatory,
+            newField_placeholder,
+            newField_type,
+            newField_value,
+          } = {},
+        } = {},
+      } = field;
       if (id === field_id) {
-        // hardcoded to fire function only if it's this particular select field. Field-id will always have to be unique in this case because there is no check for specific form JSON.
-        if (field_id === "employment" && event.target.value === "Contract") {
-          const fn = eval(formJSON[0].function);
-          console.log("function from server, ", fn);
-          fn(event.target.value);
+        // checks for a functionTrigger property (this signifies current field may trigger a function), if it exists and the value that tiggers the function execution is the same  as the value user selected - function will run
+        if (functionTrigger && event.target.value === functionTrigger) {
+          // Types of different funtions/actions will have to be hardcoded to know how to structure the function properly and what values will be exctracted from the dynamic form object
+          if (functionType === "populate") {
+            const copyElements = { ...elements };
+            const changedFieldIndex = copyElements.fields.findIndex(
+              (field) => field.field_id === targetField
+            );
+            let changedField = copyElements.fields.filter(
+              (field) => field.field_id === targetField
+            );
+            changedField[0]["field_value"] = targetFieldValue;
+            newElements.fields.splice(changedFieldIndex, 1, changedField[0]);
+            //populates a dynamic value to a dynamically spefied field
+          }
+          if (functionType === "exposeField") {
+            // generates a new dynamic field
+            const newField = {
+              field_id: newField_id,
+              field_label: newField_label,
+              field_mandatory: newField_mandatory,
+              field_placeholder: newField_placeholder,
+              field_type: newField_type,
+              field_value: newField_value,
+            };
+            newElements.fields.push(newField);
+          }
         }
+
         if (field)
           switch (field_type) {
             case "checkbox":
@@ -43,7 +84,6 @@ function App() {
       }
       setElements(newElements);
     });
-    console.log(elements);
   };
 
   return (
@@ -69,3 +109,5 @@ function App() {
 }
 
 export default App;
+
+//"function": "() => { let elementsCopy = {...elements} let fieldToPopulate = elementsCopy.//fields.filter(field => field.field_id === 'name') fieldToPopulate[0].field_value = 'TEST'}",
--- src/components/elements/Input.js
+++ src/components/elements/Input.js
@@ -1,19 +1,27 @@
-import React, { useContext } from 'react'
-import { FormContext } from '../../FormContext';
+import React, { useContext } from "react";
+import { FormContext } from "../../FormContext";
 
 const Input = ({ field_id, field_label, field_placeholder, field_value }) => {
-    const { handleChange } = useContext(FormContext)
-    return (
-        <div className="mb-3">
-            <label htmlFor="exampleInputEmail1" className="form-label">{field_label}</label>
-            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                placeholder={field_placeholder ? field_placeholder : ''}
-                value={field_value}
-                onChange={event => handleChange(field_id, event)}
-            />
-            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-        </div>
-    )
-}
+  const { handleChange } = useContext(FormContext);
+  return (
+    <div className="mb-3">
+      <label htmlFor="exampleInputEmail1" className="form-label">
+        {field_label}
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder={field_placeholder ? field_placeholder : ""}
+        value={field_value}
+        onChange={(event) => handleChange(field_id, event)}
+      />
+      <div id="emailHelp" className="form-text">
+        We'll never share your email with anyone else.
+      </div>
+    </div>
+  );
+};
 
-export default Input
+export default Input;
